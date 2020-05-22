6390a8bd86475b9e93ab60f6c41d881929d79e11
Sylvain <sylvain.durand@gmail.com>
2020-05-22 22:45:17 +0200

    Collect $MFT - optional

--- Configure.cmd
+++ Configure.cmd
@@ -4,7 +4,7 @@ set ORC_CONFIG_FOLDER=.\config
 set ORC_TOOLS_FOLDER=.\tools
 set ORC_OUTPUT_FOLDER=.\output
 
-set ORC_VERSION=v1.7
+set ORC_VERSION=v1.9
 
 if not "%1"=="" (
 	echo Configuration folder defined: %1
