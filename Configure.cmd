627db1b21954602766bbaae084baae45cb3c494c
Sylvain <sylvain.durand@gmail.com>
2020-09-15 22:19:08 +0200

    Version Release Candidate 3

--- Configure.cmd
+++ Configure.cmd
@@ -4,7 +4,7 @@ set ORC_CONFIG_FOLDER=.\config
 set ORC_TOOLS_FOLDER=.\tools
 set ORC_OUTPUT_FOLDER=.\output
 
-set ORC_VERSION=v3.1-RC2
+set ORC_VERSION=v3.1-RC3
 
 if not "%1"=="" (
 	echo Configuration folder defined: %1
